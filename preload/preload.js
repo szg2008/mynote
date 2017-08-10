a24a1043bbe6785bd3d77a326ac57ddf2c8f97eb
szg2008 <l>
2017-08-10 10:58:23 +0800

    preload

--- preload/preload.js
+++ preload/preload.js
@@ -1,24 +1,51 @@
 /**
 
-图片预加载
+图片预加载(无序加载,有序加载)
 
 */
 ;(function($){
     function PreLoad(imgs,options){
         this.imgs = typeof imgs === 'string' ? [imgs] : imgs;
         this.opts = $.extend({},PreLoad.DEFAULTS,options);
-        this._unordered();
+        if(this.opts.order == 'ordered'){
+            this._ordered();
+        }else{
+            this._unordered();
+        }
     }
     PreLoad.DEFAULTS = {
+        order:'unordered',
         each:null,//每张图片加载完成之后执行
         all:null//所有图片加载完成之后执行
+    }
+    //有序加载
+    PreLoad.prototype._ordered = function(){
+        var imgs = this.imgs,
+            opts = this.opts,
+            count = 0,
+            len = imgs.length;
+        load();
+        function load() {
+            var img = new Image();
+            $(img).on('load error',function(){
+                opts.each && opts.each(count);
+                if(count >= len){
+                    //所有图片加载完毕
+                    opts.all && opts.all();
+                }else{
+                    load();
+                }
+                count++;
+            });
+            img.src = imgs[count];
+        }
     }
     //无序加载
     PreLoad.prototype._unordered = function(){
         var imgs = this.imgs,
             opts = this.opts,
             count = 0,
-            len = imgs.length
+            len = imgs.length;
         $.each(imgs,function(index,src){
             if(typeof src != 'string'){
                 return;
